e079b7d764972236da6c77e7c38142a3e0ac7f36
Dileep <dileepkumargowda@gmail.com>
2018-06-15 17:40:01 +0530

    base space changed

--- crud/urls.py
+++ crud/urls.py
@@ -3,8 +3,53 @@ from . import views
 
 urlpatterns= [
     url(r'^$', views.index, name='index'),
-    url(r'^create$', views.create, name='create'),
-    url(r'^home/javabase$', views.home, name='home'),
-    url(r'^edit/update/(?P<id>\d+)$', views.update, name='update'),
-    url(r'^delete/(?P<id>\d+)$', views.delete, name='delete'),
+   
+    url(r'^javahome$', views.javahome, name='javahome'),
+
+    url(r'^javapartone$', views.javapartone, name='javapartone'),
+    url(r'^javaparttwo$', views.javaparttwo, name='javaparttwo'),
+    url(r'^javapartthree$', views.javapartthree, name='javapartthree'),
+    url(r'^javapartfour$', views.javapartfour, name='javapartfour'),
+
+    url(r'^javaPartOneSubOne$', views.javaPartOneSubOne, name='javaPartOneSubOne'),
+    url(r'^javaPartOneSubTwo$', views.javaPartOneSubTwo, name='javaPartOneSubTwo'),
+    url(r'^javaPartOneSubThree$', views.javaPartOneSubThree, name='javaPartOneSubThree'),
+
+    url(r'^javaPartTwoSubOne$', views.javaPartTwoSubOne, name='javaPartTwoSubOne'),
+    url(r'^javaPartTwoSubTwo$', views.javaPartTwoSubTwo, name='javaPartTwoSubTwo'),
+    url(r'^javaPartTwoSubThree$', views.javaPartTwoSubThree, name='javaPartTwoSubThree'),
+    url(r'^javaPartTwoSubFour$', views.javaPartTwoSubFour, name='javaPartTwoSubFour'),
+
+    url(r'^javaPartThreeSubOne$', views.javaPartThreeSubOne, name='javaPartThreeSubOne'),
+    url(r'^javaPartThreeSubTwo$', views.javaPartThreeSubTwo, name='javaPartThreeSubTwo'),
+    url(r'^javaPartThreeSubThree$', views.javaPartThreeSubThree, name='javaPartThreeSubThree'),
+    url(r'^javaPartThreeSubFour$', views.javaPartThreeSubFour, name='javaPartThreeSubFour'),
+
+    url(r'^javaPartFourSubOne$', views.javaPartFourSubOne, name='javaPartFourSubOne'),
+    url(r'^javaPartFourSubTwo$', views.javaPartFourSubTwo, name='javaPartFourSubTwo'),
+    url(r'^javaPartFourSubThree$', views.javaPartFourSubThree, name='javaPartFourSubThree'),
+    url(r'^javaPartFourSubFour$', views.javaPartFourSubFour, name='javaPartFourSubFour'),
+    
+
+    # deep leve of html final calls
+    url(r'^javaPartOneSubOne_11$', views.javaPartOneSubOne_11, name='javaPartOneSubOne_11'),
+    url(r'^javaPartOneSubOne_12$', views.javaPartOneSubOne_12, name='javaPartOneSubOne_12'),
+    url(r'^javaPartOneSubOne_13$', views.javaPartOneSubOne_13, name='javaPartOneSubOne_13'),
+    url(r'^javaPartOneSubOne_14$', views.javaPartOneSubOne_14, name='javaPartOneSubOne_14'),
+
+    url(r'^javaPartOneSubOne_21$', views.javaPartOneSubOne_21, name='javaPartOneSubOne_21'),
+    url(r'^javaPartOneSubOne_22$', views.javaPartOneSubOne_22, name='javaPartOneSubOne_22'),
+    url(r'^javaPartOneSubOne_23$', views.javaPartOneSubOne_23, name='javaPartOneSubOne_23'),
+    url(r'^javaPartOneSubOne_24$', views.javaPartOneSubOne_24, name='javaPartOneSubOne_24'),
+
+    url(r'^javaPartOneSubOne_31$', views.javaPartOneSubOne_31, name='javaPartOneSubOne_31'),
+    url(r'^javaPartOneSubOne_32$', views.javaPartOneSubOne_32, name='javaPartOneSubOne_32'),
+    url(r'^javaPartOneSubOne_33$', views.javaPartOneSubOne_33, name='javaPartOneSubOne_33'),
+    url(r'^javaPartOneSubOne_34$', views.javaPartOneSubOne_34, name='javaPartOneSubOne_34'),
+
+    url(r'^javaPartOneSubOne_41$', views.javaPartOneSubOne_41, name='javaPartOneSubOne_41'),
+    url(r'^javaPartOneSubOne_42$', views.javaPartOneSubOne_42, name='javaPartOneSubOne_42'),
+    url(r'^javaPartOneSubOne_43$', views.javaPartOneSubOne_43, name='javaPartOneSubOne_43'),
+    url(r'^javaPartOneSubOne_44$', views.javaPartOneSubOne_44, name='javaPartOneSubOne_44'),
+
 ]
--- crud/views.py
+++ crud/views.py
@@ -2,30 +2,169 @@ from django.shortcuts import render, redirect
 from .models import Member
 
 # Create your views here.
+'''' Views for home button or sitelogo click or index page at starting point of site '''
 
 def index(request):
-    members = Member.objects.all()
-    context = {'members': members}
-    return render(request, 'crud/index.html', context)
+    return render(request, 'crud/core/home.html', {})
 
-def create(request):
-    member = Member(firstname=request.POST['firstname'], lastname=request.POST['lastname'])
-    member.save()
+def homepage(request):
     return redirect('/')
+    
 
-def home(request):
-    members = Member.objects.all()
-    context = {'members': members}
-    return render(request, 'crud/javabase.html', context)
+#============================JAVA MENU==================================
+'''' Views for &Group::JAVA '''
 
-def update(request, id):
-    member = Member.objects.get(id=id)
-    member.firstname = request.POST['firstname']
-    member.lastname = request.POST['lastname']
-    member.save()
-    return redirect('/crud/')
 
-def delete(request, id):
-    member = Member.objects.get(id=id)
-    member.delete()
-    return redirect('/crud/')
+'''' Views for &Group::JAVA->JAVAHOME '''
+def javahome(request):
+    return render(request,'crud/java/home/javahome.html',{})
+
+
+                #============================JAVA PART-1 ===========================#
+'''' Views for &Group::JAVA->JAVAPARTS '''
+def javapartone(request):
+    return render(request, 'crud/java/parts/part-one/part-one.html', {})
+
+
+'''' Views for &Group::JAVA->JAVAPARTS->JAVASUBHEADS '''
+def javaPartOneSubOne(request):
+    return render(request, 'crud/java/parts/part-one/sub-one/part-one-sub-one.html', {})
+
+
+
+def javaPartOneSubOne_11(request):
+    return render(request, 'crud/java/parts/part-one/sub-one/11.html', {})
+
+def javaPartOneSubOne_12(request):
+    return render(request, 'crud/java/parts/part-one/sub-one/12.html', {})
+
+def javaPartOneSubOne_13(request):
+    return render(request, 'crud/java/parts/part-one/sub-one/13.html', {})
+
+def javaPartOneSubOne_14(request):
+    return render(request, 'crud/java/parts/part-one/sub-one/14.html', {})
+
+
+
+def javaPartOneSubOne_21(request):
+    return render(request, 'crud/java/parts/part-one/sub-one/21.html', {})
+
+def javaPartOneSubOne_22(request):
+    return render(request, 'crud/java/parts/part-one/sub-one/22.html', {})
+
+def javaPartOneSubOne_23(request):
+    return render(request, 'crud/java/parts/part-one/sub-one/23.html', {})
+
+def javaPartOneSubOne_24(request):
+    return render(request, 'crud/java/parts/part-one/sub-one/24.html', {})
+
+
+def javaPartOneSubOne_31(request):
+    return render(request, 'crud/java/parts/part-one/sub-one/31.html', {})
+
+def javaPartOneSubOne_32(request):
+    return render(request, 'crud/java/parts/part-one/sub-one/32.html', {})
+
+def javaPartOneSubOne_33(request):
+    return render(request, 'crud/java/parts/part-one/sub-one/34.html', {})
+
+def javaPartOneSubOne_34(request):
+    return render(request, 'crud/java/parts/part-one/sub-one/35.html', {})
+
+
+def javaPartOneSubOne_41(request):
+    return render(request, 'crud/java/parts/part-one/sub-one/41.html', {})
+
+def javaPartOneSubOne_42(request):
+    return render(request, 'crud/java/parts/part-one/sub-one/42.html', {})
+
+def javaPartOneSubOne_43(request):
+    return render(request, 'crud/java/parts/part-one/sub-one/43.html', {})
+
+def javaPartOneSubOne_44(request):
+    return render(request, 'crud/java/parts/part-one/sub-one/44.html', {})
+
+
+
+
+
+
+
+
+
+
+def javaPartOneSubTwo(request):
+    return render(request, 'crud/java/parts/part-one/sub-two/part-one-sub-two.html', {})
+
+def javaPartOneSubThree(request):
+    return render(request, 'crud/java/parts/part-one/sub-three/part-one-sub-three.html', {})
+
+
+
+
+                #============================JAVA PART-2 ===========================#
+'''' Views for &Group::JAVA->JAVAPARTS '''
+def javaparttwo(request):
+    return render(request, 'crud/java/parts/part-two/part-two.html', {})
+
+
+'''' Views for &Group::JAVA->JAVAPARTS->JAVASUBHEADS '''
+def javaPartTwoSubOne(request):
+    return render(request, 'crud/java/parts/part-two/sub-one/part-two-sub-one.html', {})
+
+def javaPartTwoSubTwo(request):
+    return render(request, 'crud/java/parts/part-two/sub-two/part-two-sub-two.html', {})
+
+def javaPartTwoSubThree(request):
+    return render(request, 'crud/java/parts/part-two/sub-three/part-two-sub-three.html', {})
+
+def javaPartTwoSubFour(request):
+    return render(request, 'crud/java/parts/part-two/sub-four/part-two-sub-three.html', {})
+
+   
+
+
+                  #============================JAVA PART-3 ===========================#
+'''' Views for &Group::JAVA->JAVAPARTS '''
+def javapartthree(request):
+    return render(request, 'crud/java/parts/part-three/part-three.html', {})
+
+
+'''' Views for &Group::JAVA->JAVAPARTS->JAVASUBHEADS '''
+def javaPartThreeSubOne(request):
+    return render(request, 'crud/java/parts/part-three/sub-one/part-three-sub-one.html', {})
+
+def javaPartThreeSubTwo(request):
+    return render(request, 'crud/java/parts/part-three/sub-two/part-three-sub-two.html', {})
+
+def javaPartThreeSubThree(request):
+    return render(request, 'crud/java/parts/part-three/sub-three/part-three-sub-three.html', {})
+
+def javaPartThreeSubFour(request):
+    return render(request, 'crud/java/parts/part-three/sub-four/part-three-sub-three.html', {})
+
+
+
+               #============================JAVA PART-4 ===========================#
+'''' Views for &Group::JAVA->JAVAPARTS '''
+
+def javapartfour(request):
+    return render(request, 'crud/java/parts/part-four/part-four.html', {})
+
+
+'''' Views for &Group::JAVA->JAVAPARTS->JAVASUBHEADS '''
+def javaPartFourSubOne(request):
+    return render(request, 'crud/java/parts/part-four/sub-one/part-four-sub-one.html', {})
+
+def javaPartFourSubTwo(request):
+    return render(request, 'crud/java/parts/part-four/sub-two/part-four-sub-two.html', {})
+
+def javaPartFourSubThree(request):
+    return render(request, 'crud/java/parts/part-four/sub-three/part-four-sub-three.html', {})
+
+def javaPartFourSubFour(request):
+    return render(request, 'crud/java/parts/part-four/sub-four/part-four-sub-four.html', {})
+
+
+
+
